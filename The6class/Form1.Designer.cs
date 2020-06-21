9c08b56e6eba5c6d5f296b20bbadb21b6474b931
littlestarGit <2743470546@qq.com>
2020-06-21 21:17:46 +0800

    快速计算素数 利用6的倍数的两边为素数 一个数因式分解的一个因子小于等于它的算术平方根 listbox的items数为二的15次方

---
+++ The6class/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace The6class
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tetnum = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "求              以内的素数";
+            // 
+            // tetnum
+            // 
+            this.tetnum.Location = new System.Drawing.Point(191, 35);
+            this.tetnum.Name = "tetnum";
+            this.tetnum.Size = new System.Drawing.Size(134, 35);
+            this.tetnum.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(3, 175);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(714, 268);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(529, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 63);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "计算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(142, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "个数";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 454);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tetnum);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tetnum;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ The6class/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.Collections.Specialized;
+//using static System.Math;
+
+namespace The6class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<int> ACE(int a)
+        {
+           
+            List<int> list = new List<int>();
+            int num = 1;
+            int m = 0;
+            int k = 6;
+            bool stp = false;
+            while (num<a)
+            {
+                if (num < 3)
+                {
+                    num++;
+                }
+                else
+                {
+                    m = k;
+                    if(stp==false)
+                    {
+                        num = m - 1;
+                    }
+                    else
+                    {
+                        num = m + 1; 
+                        k = k + 6;
+                    }
+                    stp = stp ^ true;
+                    if(num>a)
+                    {
+                        break;
+                    }
+                }
+          
+           
+                bool flag = true;
+                if (num < 6)
+                {
+                    if (num != 2 && num != 3 && num != 5)
+                    {
+                        flag = false;
+                    }
+                }
+                else
+                {
+                    int sqrt = (int)Math.Sqrt(num)+1;
+                    for (int i = 0; i <=sqrt; i += 6)
+                    {
+                        if (i == 0)
+                        {
+                            if (/*num % 2 == 0 || num % 3 == 0 || */num %5==0)
+                            {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        else if (num % (i - 1) == 0 || num % (i + 1) == 0)
+                        {
+                            flag = false;
+                            break;
+                        }
+
+                    }
+                }
+
+                if (flag)
+                { list.Add(num); }
+            }
+           
+            return list;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            label3.Text = "0";
+            int a = int.Parse(this.tetnum.Text);
+            List<int> jg = ACE(a);
+            this.label3.Text = jg.Count().ToString();
+            int ace = 0;string zf = "";
+            foreach(int num in jg)
+            {
+
+                zf += num + ",";
+                ace++;
+                if (ace % 10 == 0 || ace == jg.Count())
+                {
+                    listBox1.Items.Add(zf);
+                    zf = "";
+                }
+
+            }
+            
+        }
+    }
+}
